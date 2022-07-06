2378b32b0540a33874f442b6a814306fce6eeb1a
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-06 20:52:54 -0300

    Adicionado Dependências do Projeto e Base do Servidor

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+require('dotenv').config();
+
+//  Iniciando o servidor
+const app = express();
+
+//  Determinar o uso de JSON
+app.use(express.json());
+
+//  Conectando com o banco de dados
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//  Fazendo o require da pasta models
+//  requireDir('./models');
+
+//  Fazendo o require das rotas
+//  app.use('/api', require('./routers/index.routes'));
+
+//  Iniciando o servidor
+app.listen(3001);
+console.log('Servidor rodando em http://localhost:3001');
