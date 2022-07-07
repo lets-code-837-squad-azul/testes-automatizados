2dad6b9749ef77bccae3f4d033a56987652204db
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-07-06 23:48:31 -0300

    Início construção BD

---
+++ src/models/Alunos.js
@@ -0,0 +1,65 @@
+//Criando o Schema
+
+const mongoose = require('mongoose');
+// const mongoosePaginate = require('mongoose-paginate');
+
+const AlunoSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
+  telefone: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
+  dataNascimento: {
+    type: Date,
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: false,
+  },
+  municipio: {
+    type: String,
+    required: false,
+  },
+  nivelEscolaridade: {
+    type: String,
+    required: false,
+  },
+  etnia: {
+    type: String,
+    required: false,
+  },
+  genero: {
+    type: String,
+    required: false,
+  },
+  orientacaoSexual: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Registrando o mongoose paginate da Aplicação
+// UserSchema.plugin(mongoosePaginate);
+
+// Registrando o schema
+module.exports = mongoose.model('Aluno', AlunoSchema);
