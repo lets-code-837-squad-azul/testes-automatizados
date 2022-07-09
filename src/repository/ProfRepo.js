cae6fc7ae7c33c54c1ed356d80c47da7a2a7111f
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-09 17:29:36 -0300

    Removido Arquivos Não Utilizados

--- src/repository/ProfRepo.js
+++
@@ -1,9 +0,0 @@
-const Professor = require('../models/Professores')
-
-const setProfessor = async (objeto) => {
-    return await Professor.create(objeto)
-}
-
-const getProfessors = async (page) => {
-    return Professor.paginate({}, {page, limit: 10})
-}
